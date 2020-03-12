d3981e1c0553172ac6cf2f54467a864af236d310
Derek Mar <dmar19@uw.edu>
2020-03-11 19:58:19 -0700

    updated

--- frequency_1.py
+++ frequency_1.py
@@ -6,25 +6,33 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def frequency_count(file):
-    """This function returns the total frequency count per the file stack. It requires
-    just a file input from the user."""
-    # Reading video file
-    img = cv2.imread(file)
-    # Converting to grayscale
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    frequency = np.zeros(gray.shape[0]) #assumes image sizes are the same
-    index_count = []
-    for frame in range(len(gray)):
-        thresh = 1
+    # Reading video file and convert to grayscale
+    ret, img = cv2.imreadmulti(file, flags = cv2.IMREAD_GRAYSCALE)
+    frequency = np.zeros(np.shape(img[0])) #assumes image sizes are the same
+    index_count_row = []
+    index_count_col = []
+    for frame in range(len(img)):
+        frame_std = np.std(img[frame])*3 #calculates threshold based on noise
+        frame_mean = np.mean(img[frame])
+        thresh = frame_mean + frame_std
         #empty matrix to store counts
-        if len(np.where(gray[frame] == thresh)) > 0: #if value exceeds threshold
-            itemarray = np.where(gray[frame] >= thresh) #grab position of value
-            frequency[itemarray] = 1 #adds counter of 1 in specific location relevant to image
-            index_count = [itemarray]
-            if frame > 0 and len(np.intersect1d(index_count, itemarray)) > 0:
+        if len(np.where(img[frame] == thresh)) > 0: #if value exceeds threshold
+            row, col = np.where(img[frame] >= thresh) #grab position of value
+            for i in range(len(row)):
+                frequency[row[i], col[i]] = 1 #adds counter of 1 in specific location relevant to image
+                index_count_row = row[i]
+                index_count_col = col[i]
+            if frame > 0 and len(np.intersect1d(index_count_row, row)) > 0 and len(np.intersect1d(index_count_col, col)) > 0:
         #checks if a signal in a ROI occurs and if so, can add to the frequency in that location
-                overlap = np.intersect1d(index_count, itemarray)
-                frequency[overlap] += 1
+                overlap_row = np.intersect1d(index_count_row, row)
+                overlap_col = np.intersect1d(index_count_col, col)
+                if len(overlap_row) > 1 and len(overlap_col) > 1:
+                    for j in len(overlap_row):
+                        frequency[overlap_row[j], overlap_col[j]] += 1
+                elif len(overlap_row) == 1 and len(overlap_col) == 1:
+                    frequency[overlap_row[0], overlap_col[0]] += 1
+                else:
+                    pass
             else:
                 pass
         else:
@@ -40,7 +48,6 @@ def heatmap(file, savefig, path):
     img = cv2.imread(file)
     # Converting to grayscale
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    #frequency = np.zeros(gray.shape[0]) #assumes image sizes are the same
     pixel_x = np.linspace(0, len(gray), 32) #pixel limit set by conversion
     pixel_y = np.linspace(0, len(gray), 16)
     frequency_z = frequency_values.reshape(len(pixel_y), len(pixel_x)) #reshape to x,y
