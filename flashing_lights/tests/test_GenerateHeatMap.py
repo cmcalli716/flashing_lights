5cbea0f527b5ef723f23b9459d5ba6d97f6eb64b
Christopher McAllister <mcalli75@uw.edu>
2020-03-13 12:37:57 -0700

    Updated the test fn in theis branch, set up __init__.py

--- flashing_lights/tests/test_GenerateHeatMap.py
+++ flashing_lights/tests/test_GenerateHeatMap.py
@@ -21,7 +21,8 @@ def test_GetFreqCounts():
 
 def test_GetFreqArray():
     test_video = NP_video2
-    test_ret, test_img = cv2.imreadmulti(test_video, flags = cv2.IMREAD_GRAYSCALE)
+    test_ret, test_img = cv2.imreadmulti(test_video,
+    flags = cv2.IMREAD_GRAYSCALE)
     test_fn = GenerateHeatMap.GetFreqArray(test_video)
     # Testing output size
     assert len(test_fn) == len(test_img[0]),\
@@ -31,3 +32,19 @@ def test_GetFreqArray():
     "Output is the wrong type"
     assert np.mean(test_fn) > 0,\
     "No counts were found in test video"
+
+
+def test_Heatmap():
+    test_video = NP_video2
+    test_img_name = 'test'
+    test_img_path = '/mnt/c/Users/'
+    test_fn = GenerateHeatMap.Heatmap(test_video, test_img_path, test_img_name)
+    # Checking to see if saved file exists
+    assert os.path.exists(test_img_path) == True,\
+    "Invalid path"
+    # Checking to see if saved file exists within in the specificed path
+    assert os.path.isfile(test_img_name + '.png') == True,\
+    "Item was saved as a file."
+    # Checking to see if saved file is empty or not
+    assert os.path.getsize(test_img_path + '/' + test_img_name) > 0,\
+    "Saved file is empty."
