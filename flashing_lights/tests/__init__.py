c26b0f2cd9e4c7166f4f4042c621652dc9e32022
Derek Mar <dmar19@uw.edu>
2020-03-13 21:33:33 -0700

    updated for flake8 compliance

--- flashing_lights/tests/__init__.py
+++ flashing_lights/tests/__init__.py
@@ -1,4 +1,6 @@
-version = '1.0'
+from .version import __version__
 from .test_GenerateHeatMap import test_GetFreqCounts
 from .test_GenerateHeatMap import test_GetFreqArray
 from .test_GenerateHeatMap import test_Heatmap
+
+__all__ = [__version__]
