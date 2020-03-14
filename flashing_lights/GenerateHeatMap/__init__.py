abbe4330cab778298fd127089dcebf449df89a46
Christopher McAllister <mcalli75@uw.edu>
2020-03-13 21:24:54 -0700

    cleared __init__.py to see if it fixes execution error

--- flashing_lights/GenerateHeatMap/__init__.py
+++ flashing_lights/GenerateHeatMap/__init__.py
@@ -1,4 +1,1 @@
-__version__ = "1.0
-from .GenerateHeatMap import GetFreqCounts
-from .GenerateHeatMap import GetFreqArray
-from .GenerateHeatMap import Heatmap
+
--- flashing_lights/tests/__init__.py
+++ flashing_lights/tests/__init__.py
@@ -1,4 +1,1 @@
-version = '1.0'
-from .test_GenerateHeatMap import test_GetFreqCounts
-from .test_GenerateHeatMap import test_GetFreqArray
-from .test_GenerateHeatMap import test_Heatmap
+
