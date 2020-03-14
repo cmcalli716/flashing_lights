c5a9b4c058132b3c0316d821312fa2250d67f990
Christopher McAllister <mcalli75@uw.edu>
2020-03-14 15:28:46 -0700

    continuing to update and modify

--- flashing_lights/__init__.py
+++ flashing_lights/__init__.py
@@ -1,4 +1,2 @@
 __version__ = "1.0"
-from GenerateHeatMap import GetFreqCounts
-from GenerateHeatMap import GetFreqArray
-from GenerateHeatMap import Heatmap
+import GenerateHeatMap
--- flashing_lights/tests/__init__.py
+++ flashing_lights/tests/__init__.py
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
-
-=======
-from .version import __version__
-from .test_GenerateHeatMap import test_GetFreqCounts
-from .test_GenerateHeatMap import test_GetFreqArray
-from .test_GenerateHeatMap import test_Heatmap
-
-__all__ = [__version__]
->>>>>>> ed7e87e43393ef04fc322059c27b7878311c9695
+import __version__
+import test_GenerateHeatMap
+from test_GenerateHeatMap import test_GetFreqCounts
+from test_GenerateHeatMap import test_GetFreqArray
+from test_GenerateHeatMap import test_Heatmap
