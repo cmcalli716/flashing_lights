898dcbb4a478f751f7a744227ca7755feced15ca
Derek Mar <dmar19@uw.edu>
2020-03-14 18:51:10 -0700

    updated to reflect video decision

--- flashing_lights/tests/test_GenerateHeatMap.py
+++ flashing_lights/tests/test_GenerateHeatMap.py
@@ -3,17 +3,10 @@ import matplotlib.pyplot as plt
 import cv2
 import os
 from flashing_lights import GenerateHeatMap
-import requests
 
 
 def test_GetFreqCounts():
-    url = 'https://drive.google.com/open?id=1_QdBC2IoNTqGhwI5b9mVSK-13hiPy7nv'
-    req_test = requests.get(url)
-    assert req_test.status_code == 200,\
-        "Download failed"
-    with open('NP_video', 'wb') as f:
-        f.write(req_test.content)
-    test_ret, test_img = cv2.imreadmulti('NP_video',
+    test_ret, test_img = cv2.imreadmulti('NP_test1.tif',
                                          flags=cv2.IMREAD_GRAYSCALE)
     test_thresh = 2
     test_fn = GenerateHeatMap.GetFreqCounts(test_img[0], test_thresh)
@@ -28,14 +21,8 @@ def test_GetFreqCounts():
 
 
 def test_GetFreqArray():
-    url = 'https://drive.google.com/open?id=1mJFNPfbdzfQ6NuFcVwRL8FVMmbw3L43K'
-    req_test = requests.get(url)
-    assert req_test.status_code == 200,\
-        "Download failed"
-    with open('test_video', 'wb') as f:
-        f.write(req_test.content)
-    test_ret, test_img = cv2.imreadmulti('test_video')
-    test_fn = GenerateHeatMap.GetFreqArray('test_video')
+    test_ret, test_img = cv2.imreadmulti('NP_test1.tif')
+    test_fn = GenerateHeatMap.GetFreqArray('NP_test1.tif')
     # Testing output size
     assert len(test_fn) == len(test_img[0]),\
         "Output is the wrong shape"
@@ -47,17 +34,12 @@ def test_GetFreqArray():
 
 
 def test_Heatmap():
-    url = 'https://drive.google.com/open?id=1mJFNPfbdzfQ6NuFcVwRL8FVMmbw3L43K'
-    req_test = requests.get(url)
-    assert req_test.status_code == 200,\
-        "Download failed"
-    with open('video', 'wb') as f:
-        f.write(req_test.content)
     test_img_name = 'test'
     test_img_path = '/mnt/c/Users/'
     # Checking how many plots are made before and after function
     plot_before = plt.gcf().number
-    test_fn = GenerateHeatMap.Heatmap('video', test_img_path, test_img_name)
+    test_fn = GenerateHeatMap.Heatmap('NP_test1.tif',
+                                      test_img_path, test_img_name)
     plot_after = plt.gcf().number
     assert plot_before < plot_after,\
         "You have nothing plotted"
