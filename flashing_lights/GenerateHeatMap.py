eea3809c8e157bf7a9ca078e1e59d9d5f9d37ba4
Christopher McAllister <mcalli75@uw.edu>
2020-03-15 23:16:01 -0700

    Updated tests and frequency map code

--- flashing_lights/GenerateHeatMap.py
+++ flashing_lights/GenerateHeatMap.py
@@ -47,9 +47,9 @@ def GetFreqCounts(frame, threshold):
     return frequency
 
 
-def GetFreqArray(videofile, threshold, scale_percent):
+def GetFreqArray(videofile, threshold, scale_percent, outliers=True):
     """Finds pixel coordinates within a videofile (.tif, .mp4) for pixels
-    that are abovea calculated brightness threshold, then accumulates the
+    that are above a calculated brightness threshold, then accumulates the
     brightness event count for each coordinate,
     outputting it as a 2-D array in the same size as the video frames
 
@@ -82,39 +82,43 @@ def GetFreqArray(videofile, threshold, scale_percent):
                     freq_array[row[i], col[j]] += 1
         else:
             pass
-    # Videos may contain points with extremely high frequency
-    # relative to other pixels. This can affect the quality
-    # of the heatmap produced. So, this part of the code determines a
-    # max frequency from the freq distribution, then sets all points above
-    # the max frequency to the max frequency.
-    # This is solely for heatmap quality, it can be commented out.
-    #
-    # Creating empty frequency list for determining outliers
-    freq_list = []
-    # finding points where the frequency is greater than 0
-    freq_row, freq_col = np.where(freq_array >= 1)
-    for i in range(len(freq_row)):
-        # Appending frequency values above 0 to freq_list
-        freq_list.append(freq_array[freq_row[i], freq_col[i]])
-    # Sorting the list to calculate its distribution
-    sort_list = sorted(freq_list)
-    # Calculating the distribution
-    q1, q3 = np.percentile(sort_list, 25), np.percentile(sort_list, 75)
-    iqr = q3 - q1
-    upper_lim = q3 + 1.5*iqr
-    # Finding Outliers
-    if len(np.where(freq_array >= upper_lim)) > 0:
-        outlier_row, outlier_col = np.where(freq_array >= upper_lim)
-        for i in range(len(outlier_row)):
-            for j in range(len(outlier_col)):
-                # Replacing outlier frequency with upper limit of distribution
-                freq_array[outlier_row[i], outlier_col[j]] = upper_lim
+    if outliers == True:
+        # Videos may contain points with extremely high frequency
+        # relative to other pixels. This can affect the quality
+        # of the heatmap produced. So, this part of the code determines a
+        # max frequency from the freq distribution, then sets all points above
+        # the max frequency to the max frequency.
+        # This is solely for heatmap quality, it can be commented out.
+        #
+        # Creating empty frequency list for determining outliers
+        freq_list = []
+        # finding points where the frequency is greater than 0
+        freq_row, freq_col = np.where(freq_array >= 1)
+        for i in range(len(freq_row)):
+            # Appending frequency values above 0 to freq_list
+            freq_list.append(freq_array[freq_row[i], freq_col[i]])
+        # Sorting the list to calculate its distribution
+        sort_list = sorted(freq_list)
+        # Calculating the distribution
+        q1, q3 = np.percentile(sort_list, 25), np.percentile(sort_list, 75)
+        iqr = q3 - q1
+        upper_lim = q3 + 1.5*iqr
+        # Finding Outliers
+        if len(np.where(freq_array >= upper_lim)) > 0:
+            outlier_row, outlier_col = np.where(freq_array >= upper_lim)
+            for i in range(len(outlier_row)):
+                for j in range(len(outlier_col)):
+                    # Replacing outlier frequency with upper limit
+                    freq_array[outlier_row[i], outlier_col[j]] = upper_lim
+        else:
+            pass
     else:
         pass
     return freq_array
 
 
-def Heatmap(videofile, threshold, scale_percent, img_path, img_name):
+def Heatmap(videofile, threshold, scale_percent, img_path, img_name,
+            outliers=True):
     """Takes frequency accumulation array from
     GenerateHeatMap.GetFreqArray() and plots it as
     a colored meshgrid.
@@ -124,7 +128,10 @@ def Heatmap(videofile, threshold, scale_percent, img_path, img_name):
     # Reading video file
     ret, img = cv2.imreadmulti(videofile, flags=cv2.IMREAD_GRAYSCALE)
     # obtaining frequency array
-    z = GetFreqArray(videofile, threshold, scale_percent)
+    if outliers == True:
+        z = GetFreqArray(videofile, threshold, scale_percent)
+    else:
+        z = GetFreqArray(videofile, threshold, scale_percent, outliers=False)
     # Generating x and y axes in shape of image frame
     width = int(img[0].shape[1] * scale_percent / 100)
     height = int(img[0].shape[0] * scale_percent / 100)
--- flashing_lights/tests/test_GenerateIntensityMap.py
+++ flashing_lights/tests/test_GenerateIntensityMap.py
@@ -1,68 +1,87 @@
 import os
 import unittest
 
+import matplotlib
 import numpy as np
 import cv2
 
 from flashing_lights import GenerateIntensityMap
 
 
-data_path = os.path.join(flashing_lights.__path__[0], 'data')
+data='data/July_test.tif'
 
 
 class test_GenerateIntensityMap(unittest.TestCase):
 
     def test_GetIntensityValues(self):
         # Get video from repo for testing
-        filename = 'July_test.tif'
-        test_vid = os.path.join(data_path, filename)
+        test_vid = data
         test_ret, test_img = cv2.imreadmulti(test_vid,
                                              flags=cv2.IMREAD_GRAYSCALE)
+        # Setting Resizing Dimensions
+        scale_percent = 1
+        width = int(test_img[0].shape[1] * scale_percent / 100)
+        height = int(test_img[0].shape[0] * scale_percent / 100)
+        dim = (width, height)
+        test_img_resized = cv2.resize(test_img_resized, dim,
+                                      interpolation=cv2.INTER_AREA)
         test_thresh = 5
-        test_fn = GenerateIntensityMap.GetIntensityValues(test_img[0],
+        test_fn = GenerateIntensityMap.GetIntensityValues(test_img_resized,
                                                           test_thresh)
         # Testing output size
-        assert len(test_fn) == len(test_img[0]),\
+        assert len(test_fn) == len(test_img_resized),\
             "Output is the wrong shape"
         # Testing output type
         assert type(test_fn) == np.ndarray,\
             "Output is the wrong type"
-        assert np.mean(test_fn) > 0,\
-            "No counts were found in test image"
 
     def test_GetIntensityArray(self):
         # Get video from repo for testing
-        filename = 'July_test.tif'
-        test_vid = os.path.join(data_path, filename)
+        test_vid = data
         test_ret, test_img = cv2.imreadmulti(test_vid,
                                              flags=cv2.IMREAD_GRAYSCALE)
+        # Setting Resizing Dimensions
+        scale_percent = 1
+        width = int(test_img[0].shape[1] * scale_percent / 100)
+        height = int(test_img[0].shape[0] * scale_percent / 100)
+        dim = (width, height)
+        test_img_resized = cv2.resize(test_img[0], dim,
+                                      interpolation=cv2.INTER_AREA)
         test_thresh = 5
-        scale = 0.1
-        test_fn = GenerateIntensityMap.GetIntensityArray(test_vid,
+        scale = 1
+        test_fn = GenerateIntensityMap.GetIntensityArray(test_img_resized,
                                                          test_thresh, scale)
         # Testing output size
-        assert len(test_fn) == len(test_img[0]),\
+        assert len(test_fn) == len(test_img_resized),\
             "Output is the wrong shape"
         # Testing output type
         assert type(test_fn) == np.ndarray,\
             "Output is the wrong type"
-        assert np.mean(test_fn) > 0,\
-            "No counts were found in test video"
 
     def test_IntensityMap(self):
-        test_img_name = 'test'
-        test_img_path = '/mnt/c/Users/'
         # Get video from repo for testing
-        filename = 'July_test.tif'
-        test_vid = os.path.join(data_path, filename)
+        test_vid = data
+        test_ret, test_img = cv2.imreadmulti(test_vid,
+                                             flags=cv2.IMREAD_GRAYSCALE)
+        # Setting Resizing Dimensions
+        scale_percent = 1
+        width = int(test_img[0].shape[1] * scale_percent / 100)
+        height = int(test_img[0].shape[0] * scale_percent / 100)
+        dim = (width, height)
+        test_img_resized = cv2.resize(test_img[0], dim,
+                                      interpolation=cv2.INTER_AREA)
+        test_img_name = 'test'
+        test_img_path = '~/Desktop'
         test_thresh = 5
-        scale = 0.1
         test_fn = GenerateIntensityMap.IntensityMap(test_img, test_thresh,
-                                                    scale, test_img_path,
+                                                    scale_percent,
+                                                    test_img_path,
                                                     test_img_name)
         # Checking to see if array used for plotting is multidimensional
-        assert test_fn.ndim > 0,\
-            "Wrong dimensional array used for plotting"
+        # Testing output type
+        assert type(test_fn) == matplotlib.collections.QuadMesh,\
+            "Output is the wrong type"
         # Checking to see if file is empty or not
-        assert os.path.getsize(test_img_path + '/' + test_img_name) > 0,\
+        assert os.path.getsize(test_img_path + '/' +
+                               test_img_name + '.png') > 0,\
             "Saved file is empty."
