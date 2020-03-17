9cead87ca070c0aa7e43cf407790b61a8b3c3da7
Derek Mar <dmar19@uw.edu>
2020-03-16 23:51:34 -0700

    test

--- flashing_lights/tests/test_GenerateIntensityMap.py
+++ flashing_lights/tests/test_GenerateIntensityMap.py
@@ -17,7 +17,7 @@ test_vid = os.path.join(data_path, file_name)
 class test_GenerateIntensityMap(unittest.TestCase):
 
     def test_GetIntensityValues(self):
-        assert os.stat(test_vid).stat().st_size > 0,\
+        assert os.stat(test_vid).st_size > 0,\
             "File is empty"
         test_ret, test_img = cv2.imreadmulti(test_vid,
                                              flags=cv2.IMREAD_GRAYSCALE)
@@ -47,7 +47,7 @@ class test_GenerateIntensityMap(unittest.TestCase):
         height = int(test_img[0].shape[0] * scale_percent / 100)
         dim = (width, height)
         # Checking for valid dimensions
-        self.assertIn(0, dim, msg="Invalid Dimensions")
+        self.assertNotIn(0, dim, msg="Invalid Dimensions")
         test_img_resized = cv2.resize(test_img[0], dim,
                                       interpolation=cv2.INTER_AREA)
         test_thresh = 5
@@ -65,14 +65,10 @@ class test_GenerateIntensityMap(unittest.TestCase):
         test_img_path = '.'
         scale_percent = 1
         test_thresh = 5
-        num_figures_before = matplotlib.pyplot.gcf().number
         test_fn = GenerateIntensityMap.IntensityMap(test_vid, test_thresh,
                                                     scale_percent,
                                                     test_img_path,
                                                     test_img_name)
-        num_figures_after = matplotlib.pyplot.gcf().number
-        assert num_figures_before < num_figures_after,\
-            "Plotting went astray"
         # Testing output type
         assert type(test_fn) == matplotlib.collections.QuadMesh,\
             "Output is the wrong type"
